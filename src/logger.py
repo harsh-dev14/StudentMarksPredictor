6a4ea1fe71732f21adc4836651c8c07d0666ba61
harsh-dev14 <devharsh1403@gmail.com>
2024-09-20 19:57:48 -0400

    Updated Logger

--- src/logger.py
+++ src/logger.py
@@ -11,6 +11,3 @@ logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",level=logging.INFO
 )
-
-if __name__=="__main__":
-    logging.info("Started logging")
